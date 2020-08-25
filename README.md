49e53e5547cd8204da70387f68fcce2aa461200b
Abhijat Vatsyayan <abhijat.vatsyayan@gmail.com>
2020-08-24 20:22:03 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # py4kids.l1.2020.proj.chat
+
+## Creating an environment using conda 
+
+    conda create --name py4kids.chat python=3.7.4
+    
+After installation, 
+  
+    conda activate py4kids.chat
+    pip install Flask-SocketIO  
---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@socketio.on('chat.msg', namespace='/chat')
+def test_message(message):
+    print(message['data'])
+    emit('chat.echo', {'data': message['data']}, broadcast=True)
+
+
+@socketio.on('connect', namespace='/chat')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+
+@socketio.on('disconnect', namespace='/chat')
+def test_disconnect():
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    socketio.run(app, port=8181, debug=True)
